26cce80d0f03041e082f6f3606412792060bacac
sungwoo0902 <rhtjddn2@gmail.com>
2023-06-21 10:32:18 +0900

    5장 상속 실습하기

---
+++ java/Ch05/src/sub05/Sedan.java
@@ -0,0 +1,23 @@
+package sub05;
+
+public class Sedan extends Car {
+
+	private int cc;
+	
+	
+	public Sedan(String name, String color, int speed, int cc) {
+		super(name, color, speed);
+		this.cc = cc;
+		
+	}
+	
+	public void speedTurbo() {
+		// private -> protected로 접근권한 변경해서 자식클래스 참조
+		this.speed += 20;
+	}
+	
+	public void show() {
+		super.show();
+		System.out.println("배기량 : " +cc);
+	}
+}
