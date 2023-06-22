edc2aa2dcf54e80e3daa556a07d678049bb77f00
sungwoo0902 <rhtjddn2@gmail.com>
2023-06-22 12:05:39 +0900

    다형성 실습 예제 추가

---
+++ java/Ch05/src/sub07/Tiger.java
@@ -0,0 +1,14 @@
+package sub07;
+
+public class Tiger extends Animal{
+
+	@Override
+	public void move() {
+		System.out.println("Tiger run...");
+	}
+	
+	@Override
+	public void hunt() {
+		System.out.println("Tiger hunt...");
+	}
+}
