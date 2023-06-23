d4a06a19afe3f5205b0e34631bfea36d093c8637
sungwoo0902 <rhtjddn2@gmail.com>
2023-06-23 11:00:27 +0900

    연슴문제3 풀이

---
+++ java/Textbook/src/Ch06/sec07/Car.java
@@ -0,0 +1,32 @@
+package Ch06.sec07;
+
+public class Car {
+		
+		// 필드 선언
+		String company = "현대자동차";
+		String model;
+		String color;
+		int maxSpeed;
+		
+		// 생성자 선언
+		Car(){}
+		
+		Car(String model){
+			this.model = model;
+			this.color = color;
+		}
+		
+		Car(String model, String color){
+			this.model = model;
+			this.color = color;
+		}
+		
+		Car(String model, String color, int maxSpeed){
+			this.model = model;
+			this.color = color;
+			this.maxSpeed = maxSpeed;
+		}
+
+}
+
+
---
+++ java/Textbook/src/Ch06/sec08/exam04/Calculator.java
@@ -0,0 +1,12 @@
+package Ch06.sec08.exam04;
+
+public class Calculator {
+	// 정사각형의 넓이
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+}
