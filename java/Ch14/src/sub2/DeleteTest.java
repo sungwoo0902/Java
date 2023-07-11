29225b093d729614a59b4d4fa5a0a723e6eb25e3
sungwoo0902 <rhtjddn2@gmail.com>
2023-07-11 10:36:52 +0900

    14장JDBC Prepared 실습하기

---
+++ java/Ch14/src/sub2/DeleteTest.java
@@ -0,0 +1,47 @@
+package sub2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/*
+ * 날짜 : 2023/07/10
+ * 이름 : 고성우
+ * 내용 : DELETE 실습하기
+ */
+public class DeleteTest {
+
+	public static void main(String[] args) {
+		
+		// DB 정보
+		String host = "jdbc:mysql://localhost:3306/userdb";
+		String user = "root";
+		String pass = "1234";
+		
+		try {
+			// 1단계 - 1단계 드라이버 로드
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 2단계 - 데이터베이스 접속
+			Connection conn = DriverManager.getConnection(host, user, pass);
+			
+			// 3단계 - SQL 실행 객체 생성
+			Statement stmt = conn.createStatement();
+			
+			// 4단계 - SQL 실행
+			String sql = "DELETE FROM `User1` WHERE `uid` = 'J101';";
+			stmt.executeUpdate(sql);
+				   
+			// 5단계 - 결과처리(SELECT 경우) 			
+			// 6단계 - 연결해제
+		    stmt.close();
+		    conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Delete 완료");
+	}
+
+}
---
+++ java/Ch14/src/sub3/PreparedDeleteTest.java
@@ -0,0 +1,50 @@
+package sub3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/*
+ * 날짜 : 2023/07/11
+ * 이름 : 고성우
+ * 내용 : PreparedStatement 실습하기 
+ */
+public class PreparedDeleteTest {
+
+	public static void main(String[] args) {
+		
+		// DB정보
+		String host = "jdbc:mysql://127.0.0.1:3306/userdb";
+		String user = "root";
+		String pass = "1234";
+		
+		try {
+			// 1단계 - JDCB 드라이버 로드
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 2단계 - 데이터베이스 접속
+			Connection conn = DriverManager.getConnection(host, user, pass);
+			
+			// 3단계 - SQL 실행 객체 생성
+			String sql = "DELETE FROM `User2` where `uid` = ?";
+			PreparedStatement psmt = conn.prepareStatement(sql);
+			psmt.setString(1, "j101");
+			
+			// 4단계 - SQL 실행
+			psmt.executeUpdate();
+			
+			// 5단계 - 결과처리(SELECT 경우)
+			
+			// 6단계 - 연결해제
+			psmt.close();
+			conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Delete 실행...");
+
+	}
+
+}
