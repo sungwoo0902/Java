bfae9aff8945fe50aed9c0a0558f7e349844d6c7
sungwoo0902 <rhtjddn2@gmail.com>
2023-07-21 12:45:27 +0900

    연습문제8 실습하기

---
+++ java/TestExam/src/Test8/Test01.java
@@ -0,0 +1,38 @@
+package Test8;
+
+import java.util.StringTokenizer;
+
+/*
+ * 날짜 : 2023/07/21
+ * 이름 : 고성우
+ * 내용 : 자바 총정리 연습문제
+ */
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		String[] arr1 = { "Apple", "Banana", "Grape", "Orage"};
+		String result1 = String.join("-", arr1);
+		System.out.println("result1 : " +result1);
+		
+		String[] arr2 = result1.split("-");
+		String result2 = String.join("", arr2);
+		System.out.println("result2 : " +result2);
+		
+		String[] arr3 = result2.split("(?=[A-Z])");
+		String result3 = String.join("^", arr3);
+		System.out.println("result3 : " +result3);
+		
+		StringTokenizer st = new StringTokenizer(result3, "^");
+		StringBuilder sb = new StringBuilder();
+		
+		while(st.hasMoreTokens()) {
+			sb.append(st.nextToken());
+			
+		}
+		
+		String result4 = sb.toString();
+		System.out.println("result4 : " +result4);
+	}
+
+}
---
+++ java/TestExam/src/Test8/Test02.java
@@ -0,0 +1,33 @@
+package Test8;
+
+/*
+ * 날짜 : 2023/07/21
+ * 이름 : 고성우
+ * 내용 : 자바 총정리 연습문제
+ */
+public class Test02 {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Hello";
+		String str2 = "World";
+		
+		System.out.println("str1 역순 : " +reverseStr(str1));
+		System.out.println("str1 역순 : " +reverseStr(str2));
+
+	}
+
+	private static String reverseStr(String str) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			sb.append(str.charAt(i));
+		}
+		
+		return sb.toString();	
+		
+		
+	}
+	
+}
