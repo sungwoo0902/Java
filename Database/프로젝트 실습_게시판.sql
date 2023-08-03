134de2cb7ef8c137d6744a177cd30cbbbc04f07f
sungwoo0902 <rhtjddn2@gmail.com>
2023-08-03 17:39:36 +0900

    본체 변경으로 인한 백업

---
+++ Database/프로젝트 실습_게시판.sql
@@ -0,0 +1,10 @@
+# 날짜 : 2023/08/02
+# 이름 : 고성우
+# 내용 : 게시판 프로젝트 실습하기
+
+CREATE DATABASE `Jboard`;
+
+CREATE TABLE `User`();
+CREATE TABLE `Article`();
+CREATE TABLE `File`();
+CREATE TABLE `Terms`();
